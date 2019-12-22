006fc1af0c68316dded90f48c8c47c9abbf0233c
kho903 <gmldnr2222@naver.com>
2019-12-22 22:09:26 +0900

    photos

--- users/models.py
+++ users/models.py
@@ -30,7 +30,7 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW")
     )
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, blank=True
     )
