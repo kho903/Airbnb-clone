8fcb723819a38fc0398c04783e10661561c028a0
kho903 <gmldnr2222@naver.com>
2019-12-23 22:14:34 +0900

    seed_rooms pt2

--- rooms/admin.py
+++ rooms/admin.py
@@ -93,7 +93,7 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_photos(self, obj):
         return obj.photos.count()
-
+    count_photos.short_description = "Photo Count"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
