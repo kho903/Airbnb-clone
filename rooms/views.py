d49be9b6fb6ca1f914a10c5410fdb085831b2862
kho903 <gmldnr2222@naver.com>
2019-12-28 00:43:58 +0900

    start of templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
 
 
 def all_rooms(request):
-    pass
+    now = datetime.now()
+    hungry = True
+    return render(request, "all_rooms.html", context={
+        'now': now,
+        'hungry': hungry
+    })
