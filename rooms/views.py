e96303048c03a6d5222b4c53bb6b17afe0e44f05
kho903 <gmldnr2222@naver.com>
2019-12-30 18:15:42 +0900

    Class Based Views pt2

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -10,3 +11,10 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
