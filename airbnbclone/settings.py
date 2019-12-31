48c5538f90e821b3d345279ececf383ad4a0033e
kho903 <gmldnr2222@naver.com>
2019-12-31 15:33:31 +0900

    Using DetailView CBV

--- airbnbclone/settings.py
+++ airbnbclone/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8dk)jpc4xebykm#_qr1l#+3ksjf&$3h8wydd-h*o+m$*97vkv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", views.room_detail, name="detail")
+    path("<int:pk>", views.RoomDetail.as_view(), name="detail")
 ]
