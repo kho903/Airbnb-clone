c5d96067632e5a863bdaafbf2299e6572c512f08
kho903 <gmldnr2222@naver.com>
2019-12-31 20:14:35 +0900

    Finishing the Form

--- rooms/views.py
+++ rooms/views.py
@@ -30,10 +30,12 @@ def search(request):
     price = request.GET.get("price", 0)
     guests = request.GET.get("guests", 0)
     bedrooms = request.GET.get("bedrooms", 0)
-    beds = request.GET.get("bed", 0)
+    beds = request.GET.get("beds", 0)
     baths = request.GET.get("baths", 0)
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
     form = {
         "city": city,
@@ -44,6 +46,10 @@ def search(request):
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host
     }
 
     room_types = models.RoomType.objects.all()
