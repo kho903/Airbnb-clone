e6267c155ad179aabc4346f6c1c5ce8b638ffa16
kho903 <gmldnr2222@naver.com>
2019-12-31 16:01:53 +0900

    Template, Form, Url, Setup (page_title변수명 고침)

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("<int:pk>", views.RoomDetail.as_view(), name="detail")
+    path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
+    path("search/", views.search, name="search"),
 ]
