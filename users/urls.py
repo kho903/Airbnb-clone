06cdb05a97e8d93540755fe9a8edd90734746389
kho903 <gmldnr2222@naver.com>
2020-01-03 18:03:33 +0900

    Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login")
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout")
 ]
