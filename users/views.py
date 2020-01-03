ad3eaa8ebd47d20983da25f24432827bce553129
kho903 <gmldnr2222@naver.com>
2020-01-03 20:07:16 +0900

    Sign Up Form pt.2

--- users/views.py
+++ users/views.py
@@ -37,3 +37,12 @@ class SignUpView(FormView):
         "last_name": "JIHUN",
         "email": "gmldnr2222@naver.com",
     }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
