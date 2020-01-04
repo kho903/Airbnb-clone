6fe6da0a089e5e53d7c4c62178b3493bb54904b4
kho903 <gmldnr2222@naver.com>
2020-01-04 16:43:43 +0900

    Veryfying Email pt1 --SMTPAuthenticationError 아직 해결 X

--- airbnbclone/settings.py
+++ airbnbclone/settings.py
@@ -141,3 +141,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "huni@sandboxb10248765cad4cad996339581e3eae29.mailgun.org"
