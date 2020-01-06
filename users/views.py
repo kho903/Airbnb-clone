4bc4e6722c3286e60d55ae5c362abf9a2309e3ea
kho903 <gmldnr2222@naver.com>
2020-01-06 18:28:48 +0900

    Getting Github Profile

--- users/views.py
+++ users/views.py
@@ -67,7 +67,8 @@ def complete_verification(request, key):
 def github_login(request):
     client_id = os.environ.get("GH_ID")
     redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
-    return redirect(f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user")
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user")
 
 
 def github_callback(request):
@@ -75,10 +76,39 @@ def github_callback(request):
     client_secret = os.environ.get("GH_SECRET")
     code = request.GET.get("code", None)
     if code is not None:
-        request = requests.post(
+        result = requests.post(
             f"http://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
             headers={"Accept": "application/json"}
         )
-        print(request.json())
+        result_json = result.json()
+        error = result_json.get("error", None)
+        if error is not None:
+            return redirect(reverse("users:login"))
+        else:
+            access_token = result_json.get("access_token")
+            profile_request = requests.get(
+                "https://api.github.com/user",
+                headers={
+                    "Authorization": f"token {access_token}",
+                    "Accept": "application/json"
+                },
+            )
+            profile_json = profile_request.json()
+            username = profile_json.get("login", None)
+            if username is not None:
+                name = profile_json.get('name')
+                email = profile_json.get('email')
+                bio = profile_json.get('bio')
+                user = models.User.objects.get(email=email)
+                if user is not None:
+                    return redirect(reverse("users:login"))
+                else:
+                    user = models.User.objects.get(
+                        username=email, first_name=name, bio=bio, email=email
+                    )
+                    login(request, user)
+                    return redirect(reverse("core:home"))
+            else:
+                return redirect(reverse("users:login"))
     else:
         return redirect(reverse("core:home"))
