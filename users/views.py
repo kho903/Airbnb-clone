7c398ccec51760cb85acfd6e0a807c17329eb2f7
kho903 <gmldnr2222@naver.com>
2020-01-13 00:09:22 +0900

    Sign Up

--- users/views.py
+++ users/views.py
@@ -34,11 +34,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "KIM",
-        "last_name": "JIHUN",
-        "email": "gmldnr2222@naver.com",
-    }
 
     def form_valid(self, form):
         form.save()
