fab14f0315a1a1a421060d7805735d31fb2a59fd
kho903 <gmldnr2222@naver.com>
2020-01-13 09:18:09 +0900

    Forms Shortcut

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -23,7 +22,7 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
@@ -37,6 +36,16 @@ class SignUpForm(UserCreationForm):
         widget=forms.PasswordInput(attrs={"placeholder": "Password"})
     )
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
