56a410cd48c4be326db8e799b8ebe0c1e495fd3d
kho903 <gmldnr2222@naver.com>
2020-01-14 19:09:34 +0900

    User Profile pt.2

--- users/views.py
+++ users/views.py
@@ -189,3 +189,8 @@ class UserProfileView(DetailView):
 
     model = models.User
     context_object_name = "user_obj"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hello"] = "Hello!"
+        return context
