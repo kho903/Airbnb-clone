819bee537664969e0ab6c6b81c760a6c80ec06be
kho903 <gmldnr2222@naver.com>
2020-01-15 16:24:00 +0900

    Customizing Stuff

--- users/views.py
+++ users/views.py
@@ -214,6 +214,21 @@ class UpdateProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['first_name'].widget.attrs = {"placeholder": "First Name"}
+        form.fields['last_name'].widget.attrs = {"placeholder": "Last Name"}
+        form.fields['bio'].widget.attrs = {"placeholder": "Bio"}
+        form.fields['birthdate'].widget.attrs = {"placeholder": "Birthdate"}
+        return form
+
 
 class UpdatePasswordView(PasswordChangeView):
     template_name = "users/update-password.html"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['old_password'].widget.attrs = {"placeholder": "Current Password"}
+        form.fields['new_password1'].widget.attrs = {"placeholder": "New Password"}
+        form.fields['new_password2'].widget.attrs = {"placeholder": "Confirm New Password"}
+        return form
