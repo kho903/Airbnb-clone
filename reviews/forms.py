b3ee36e73c400b8f8c4461c1bf3cb9b4c46ca6e9
kho903 <gmldnr2222@naver.com>
2020-01-18 19:57:00 +0900

    Validate

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,7 +3,12 @@ from . import models
 
 
 class CreateReviewForm(forms.ModelForm):
-
+    Accuracy = forms.IntegerField(max_value=5, min_value=1)
+    Communication = forms.IntegerField(max_value=5, min_value=1)
+    Cleanliness = forms.IntegerField(max_value=5, min_value=1)
+    Location = forms.IntegerField(max_value=5, min_value=1)
+    Check_in = forms.IntegerField(max_value=5, min_value=1)
+    Value = forms.IntegerField(max_value=5, min_value=1)
     class Meta:
         model = models.Review
         fields = (
--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 
 
@@ -6,12 +7,24 @@ class Review(core_models.TimeStampedModel):
     """ Review model Definition """
 
     review = models.TextField()
-    Accuracy = models.IntegerField()
-    Communication = models.IntegerField()
-    Cleanliness = models.IntegerField()
-    Location = models.IntegerField()
-    Check_in = models.IntegerField()
-    Value = models.IntegerField()
+    Accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    Communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    Cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    Location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    Check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    Value = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     user = models.ForeignKey("users.User", related_name="reviews", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", related_name="reviews", on_delete=models.CASCADE)
 
@@ -20,14 +33,15 @@ class Review(core_models.TimeStampedModel):
 
     def rating_average(self):
         avg = (
-            self.Accuracy +
-            self.Communication +
-            self.Cleanliness +
-            self.Location +
-            self.Check_in +
-            self.Value
-        ) / 6
+                      self.Accuracy +
+                      self.Communication +
+                      self.Cleanliness +
+                      self.Location +
+                      self.Check_in +
+                      self.Value
+              ) / 6
         return round(avg, 2)
+
     rating_average.short_description = "Avg."
 
     class Meta:
